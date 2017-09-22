56ebaa369fb8ba91a22923cf651cd46dd6680ffa
Mathias Lüstraeten <mathias.luestraeten@gmail.com>
2017-09-23 00:10:56 +0200

    Windows console commands
    Signed-off-by: Mathias Lüstraeten <mathias.luestraeten@gmail.com>

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ var express = require("express");
 
 var app = express();
 
-var jobsCMD = 'sudo bconsole << EOF \n .api 2 \n llist jobs ';
-var clientsCMD = 'sudo bconsole << EOF \n .api 2 \n .clients';
+var jobsCMD = '(echo .api 2 & echo llist jobs) | bconsole';
+var clientsCMD = '(echo .api 2 & echo .clients) | bconsole';
 
 app.use(express.static('static'));
 
 app.get("/api/clients", function(req, res) {
   cp.exec(clientsCMD, (err, stdout, stderr) => {
-  var arr=stdout.split(".clients");
-    var clients=JSON.parse(arr[1]).result.clients;
+    var arr=stdout.split(".clients");
+    var json=arr[1];
+    if (json[json.length]='*') json = json.slice(0,-1); /*trim trailing star if needed*/
+    var clients=JSON.parse(json).result.clients;
     res.send(clients);
   })
 });
@@ -19,7 +21,9 @@ app.get("/api/clients", function(req, res) {
 app.get("/api/jobs", function(req, res) {
   cp.exec(jobsCMD, (err, stdout, stderr) => {
     var arr=stdout.split("llist jobs");
-    var jobs=JSON.parse(arr[1]).result.jobs;
+    var json=arr[1];
+    if (json[json.length]='*') json = json.slice(0,-1); /*trim trailing star if needed*/
+    var jobs=JSON.parse(json).result.jobs;
     res.send(jobs);
   })
 });
